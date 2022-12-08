9185014191bed99db7589bff4f1bcf23d0c0c6ba
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2022-12-08 10:18:07 +0530

    Refactor Homepage to dynamically show products by category

--- src/Pages/Customer/Homepage/index.js
+++ src/Pages/Customer/Homepage/index.js
@@ -1,70 +1,57 @@
-import { Typography } from "@mui/material";
-import axios from "axios";
-import React from "react";
-import HomePageCarousel from "../../../Components/HomepageCarousel";
-import ItemCarousel from "../../../Components/ItemCarousel";
+import { Typography } from '@mui/material';
+import axios from 'axios';
+import React from 'react';
+import HomePageCarousel from '../../../Components/HomepageCarousel';
+import ItemCarousel from '../../../Components/ItemCarousel';
 
 function Homepage({ products, setProducts }) {
-  const [fashion, setFashion] = React.useState([]);
-  const [electronics, setElectronics] = React.useState([]);
+  const [categories, setCategories] = React.useState([]);
 
-  React.useEffect(() => {
-    const AllProducts = async () => {
-      const data = await axios.get(
-        `${process.env.REACT_APP_ROOT_URL}/product/all`,
-        {
-          headers: { Authorization: localStorage.getItem("token") },
-        }
+	const capitalize = (s) => {
+		const lower = s.toLowerCase();
+		return s.charAt(0).toUpperCase() + lower.slice(1)
+	}
+
+  React.useEffect(() => { // Get a list of all the categories 
+    const getAllCategories = async () => {
+      const response = await axios.get(
+        `${process.env.REACT_APP_ROOT_URL}/product/categories`
       );
-      setProducts(data.data);
-      // var xhr = new XMLHttpRequest();
-      // xhr.open("GET", `${process.env.REACT_APP_ROOT_URL}/product/all`);
-      // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      // // xhr.send("Username=" + username, "Password=" + password);
-      // xhr.onload = function () {
-      //   let data = JSON.parse(xhr.responseText);
-      //   console.log("Signed in as: ", data);
-      //   // localStorage.setItem("SavedToken", "Bearer " + data.token);
-      //   // setLoggedIn(true);
-      // };
+      setCategories(response.data);
+    };
+    getAllCategories();
+  }, []);
+
+  React.useEffect(() => { // Get all the products
+    const AllProducts = async () => {
+      const repsonse = await axios.get(
+        `${process.env.REACT_APP_ROOT_URL}/product/all`
+      );
+      setProducts(repsonse.data);
     };
     AllProducts();
   }, []);
 
-  const useCategory = (category, setFunc) => {
-    React.useEffect(() => {
-      axios
-        .get(`${process.env.REACT_APP_ROOT_URL}/product/${category}`, {
-          headers: { Authorization: localStorage.getItem("token") },
-        })
-        .then((response) => setFunc(response.data));
-    }, []);
-  };
-
-  useCategory("FASHION", setFashion);
-  useCategory("ELECTRONICS", setElectronics);
   return (
     <>
       <HomePageCarousel />
       <div style={{ padding: 20 }}>
         <div>
-          <Typography variant="h4" pb={2}>
+          <Typography variant='h4' pb={2}>
             New Arrivals
           </Typography>
           <ItemCarousel data={products} />
         </div>
-        <div style={{ marginTop: 30 }}>
-          <Typography variant="h4" pb={2}>
-            Top Fashion
-          </Typography>
-          <ItemCarousel data={fashion} />
-        </div>
-        <div style={{ marginTop: 30 }}>
-          <Typography variant="h4" pb={2}>
-            Top Electronics
-          </Typography>
-          <ItemCarousel data={electronics} />
-        </div>
+        {categories.map((category) => (
+          <div style={{ marginTop: 30 }} key={category}>
+            <Typography variant='h4' pb={2}>
+              Top {capitalize(category)}
+            </Typography>
+            <ItemCarousel
+              data={products.filter(product => product.category === category)}
+            />
+          </div>
+        ))}
       </div>
     </>
   );
